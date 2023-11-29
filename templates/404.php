be900e20f4738868e3d7c75a1711e85169b32bd4
W3ap0n-X <39168945+W3ap0n-X@users.noreply.github.com>
2023-11-29 14:33:19 -0700

    Create 404.php

---
+++ templates/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 404 Template
+ *
+ * This file contains the 404 template used in this theme.
+ *
+ * @since 2.0
+ * @package Crio
+ */
+?>
+
+<div class="text-center">
+	<h1><?php esc_html_e( '404: Page Not Found.', 'crio' ); ?></h1>
+		<p><?php esc_html_e( 'The page you requested could not be found.', 'crio' ); ?></p>
+</div>
